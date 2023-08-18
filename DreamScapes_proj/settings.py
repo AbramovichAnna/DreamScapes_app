0ceaf3aee6a8d61c01b70fcbba679767fc7b058a
Anna Abramovich <aabramovich1@gmail.com>
2023-08-19 01:42:20 +0300

    deploy test-2

--- DreamScapes_proj/settings.py
+++ DreamScapes_proj/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Dreamscapes_app/static"),]
-STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
